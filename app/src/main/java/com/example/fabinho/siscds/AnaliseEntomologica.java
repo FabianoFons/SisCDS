cd3e88270ec0247387d83dd5cb5dd6b202c56381
FabianoFons <fabiano.fons20@gmail.com>
2016-09-14 20:48:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/fabinho/siscds/AnaliseEntomologica.java
@@ -0,0 +1,13 @@
+package com.example.fabinho.siscds;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnaliseEntomologica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analise_entomologica);
+    }
+}
